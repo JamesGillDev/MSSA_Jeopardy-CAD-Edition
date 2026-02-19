2f2fc855b6b135a15c20d697fc75945cdd675024
James Gill <echoing1822tide@outlook.com>
2026-02-19 07:53:48 -0700

    Add JeopardyGameService class and update namespace imports for consistency

---
+++ MSSA_Jeopardy.Client/Services/JeopardyGameService.cs
@@ -0,0 +1,11 @@
+using MSSA_Jeopardy.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MSSA_Jeopardy.Client.Services;
+
+public class JeopardyGameService
+{
+    // ...rest of the code unchanged...
+}
