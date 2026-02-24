d6db241e38c1f404076df8387ee809239d5416dc
James Gill <echoing1822tide@outlook.com>
2026-02-24 10:20:40 -0700

    Enhance Blazor startup handling with error UI auto-reload and detailed error options

--- MSSA Jeopardy/Program.cs
+++ MSSA Jeopardy/Program.cs
@@ -5,7 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DetailedErrors = builder.Environment.IsDevelopment();
+    })
+    .AddHubOptions(options =>
+    {
+        options.HandshakeTimeout = TimeSpan.FromSeconds(30);
+        options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+        options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
+    });
 
 builder.Services.AddSingleton<JeopardyGameService>();
 
